c800c0e04a40daf9afa69f592d971ba67359e4b6
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-12 20:36:31 -0300

    feat: Cria as controllers de Usuario e Chamado

---
+++ src/main/java/dev/bieelg18/APIChamados/chamado/ChamadoController.java
@@ -0,0 +1,82 @@
+package dev.bieelg18.APIChamados.chamado;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chamado")
+@RequiredArgsConstructor
+public class ChamadoController {
+
+    private final ChamadoService chamadoService;
+
+
+    //Rota para criar um chamado (Todos podem acessar, só precisa estar autenticado)
+    @PostMapping
+    public ListarChamadosDTO criarChamado(@RequestBody CriarChamadoDTO criarDTO){
+        return chamadoService.criarChamado(criarDTO);
+    }
+
+    //Rota para listar todos os chamados (Precisa estar autenticado e ser nivel suporte)
+    @GetMapping
+    public List<ListarChamadosDTO> listarChamados(){
+        return chamadoService.listarChamados();
+    }
+
+    //Rota para acessar os chamados de quem chamou a requisição (Todos podem acessar estando autenticado - Ainda precisa implementar a parte de saber quem chamou no método)
+    @GetMapping("/meusChamados")
+    public List<ListarChamadosDTO> chamadosUsuario(){
+        return chamadoService.listarChamadosUsuario();
+    }
+
+    //Rota para listar chamados em aberto (Precisa estar autenticado e ser nivel suporte)
+    @GetMapping("/aberto")
+    public List<ListarChamadosDTO> abertos(){
+        return chamadoService.chamadosEmAberto();
+    }
+
+    //Rota para listar chamados fechados (Precisa estar autenticado e ser nivel suporte)
+    @GetMapping("/fechado")
+    public List<ListarChamadosDTO> fechados(){
+        return chamadoService.chamadosEncerrados();
+    }
+
+    //Rota para listar chamados em atendimento (Precisa estar autenticado e ser nivel suporte)
+    @GetMapping("/atendimento")
+    public List<ListarChamadosDTO> emAtendimento(){
+        return chamadoService.chamadosEmAtendimento();
+    }
+
+    //Rota para listar chamados improcedentes (Precisa estar autenticado e ser nivel suporte)
+    @GetMapping("/improcedente")
+    public List<ListarChamadosDTO> improcedentes(){
+        return chamadoService.chamadosImprocedentes();
+    }
+
+    //Rota para alterar um chamado de aberto para em atendimento (Precisa estar autenticado e ser nivel suporte)
+    @PatchMapping("/atendimento/{id}")
+    public ListarChamadosDTO atendimento(@PathVariable Integer id){
+        return chamadoService.emAtendimento(id);
+    }
+
+    //Rota para fechar um chamado (Precisa estar autenticado e ser nivel suporte)
+    @PatchMapping("/fechar/{id}")
+    public ListarChamadosDTO fecharChamado(@PathVariable Integer id){
+        return chamadoService.fecharChamado(id);
+    }
+
+    //Rota para alterar chamado para improcedente (Precisa estar autenticado e ser nivel suporte)
+    @PatchMapping("/improcedente/{id}")
+    public ListarChamadosDTO improcedente(@PathVariable Integer id){
+        return chamadoService.chamadoImprocedente(id);
+    }
+
+    //Rota para deletar um chamado (Precisa estar autenticado e ser nivel suporte)
+    @DeleteMapping("/{id}")
+    public void deletarChamado(@PathVariable Integer id){
+        chamadoService.deletarChamado(id);
+    }
+
+}
---
+++ src/main/java/dev/bieelg18/APIChamados/usuario/UsuarioController.java
@@ -0,0 +1,52 @@
+package dev.bieelg18.APIChamados.usuario;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+
+    //Rota para se cadastrar (Todos podem acessar e não precisa estar autenticado)
+    @PostMapping
+    public ListarUsuarioDTO criarUsuario(@RequestBody CriarUsuarioDTO criarDTO){
+        return usuarioService.criarUsuario(criarDTO);
+    }
+
+    //Rota para listar todos os usuário cadastrados (Precisa estar autenticado e ser nivel suporte)
+    @GetMapping
+    public List<ListarUsuarioDTO> usuarios(){
+        return usuarioService.listarUsuarios();
+    }
+
+    //Rota para buscar usuario por e-mail (Precisa estar autenticado e ser nivel suporte)
+    @GetMapping("/buscar")
+    public ListarUsuarioDTO buscarEmail(@RequestParam String email){
+        return usuarioService.buscarPorEmail(email);
+    }
+
+    //Rota para deletar usuarios por id (Precisa estar autenticado e ser nivel suporte)
+    @DeleteMapping("/{id}")
+    public void deletarUsuario(@PathVariable Integer id){
+        usuarioService.deletarUsuario(id);
+    }
+
+    //Rota para atualizar dados de cadastro (Todos podem acessar, só precisa estar autenticado)
+    @PatchMapping("/{id}")
+    public ListarUsuarioDTO alterarDadosCadastro(@PathVariable Integer id, @RequestBody EditarUsuarioDTO editarDTO){
+        return usuarioService.editarDadosCadastro(id, editarDTO);
+    }
+
+    //Rota para alterar a permissao de um usuario ja cadastrado (Precisa estar autenticado e ser nivel suporte)
+    @PatchMapping("/permissao/{id}")
+    public ListarUsuarioDTO editarPermissao(@PathVariable Integer id, @RequestBody EditarPermissaoDTO permissaoDTO){
+        return usuarioService.editarPermissao(id, permissaoDTO);
+    }
+
+}
